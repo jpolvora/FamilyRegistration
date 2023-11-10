740085a480d532f256fffa197a851effce7a8da1
JONE POLVORA <jpolvora@gmail.com>
2023-11-10 09:34:34 -0400

    added tests to transaction script

---
+++ FamilyRegistration.Tests/TransactionScript/TransactScriptTests.cs
@@ -0,0 +1,84 @@
+﻿using FamilyRegistration.Core.Strategy;
+using FamilyRegistration.Core.UseCases.ProcessData;
+using FluentAssertions;
+
+namespace FamilyRegistration.Tests.TransactionScript;
+public class TransactScriptTests
+{
+    [Fact]
+    public async void CalcScoreUsingTransactionScriptShouldBe7()
+    {
+        //arrange 
+        var inputItem = new InputItem()
+        {
+            Key = Guid.NewGuid().ToString(),
+            NumOfDependents = 2,
+            FamilyIncome = 900
+        };
+
+        var useCase = new ProcessDataUseCase(new ProcessDataWithTransactionScript());
+
+        //act
+        var input = new Input(new[] { inputItem });
+        var output = await useCase.Execute(input);
+
+        output.Count.Should().Be(1);
+
+        var outputItem = output.Single();
+
+        //assert
+        outputItem.Score.Should().Be(7);
+    }
+
+    [Fact]
+    public async void CalcScoreUsingTransactionScriptShouldBe5()
+    {
+        //arrange 
+        var inputItem = new InputItem()
+        {
+            Key = Guid.NewGuid().ToString(),
+            NumOfDependents = 1,
+            FamilyIncome = 901
+        };
+
+        var useCase = new ProcessDataUseCase(new ProcessDataWithTransactionScript());
+
+        //act
+        var input = new Input(new[] { inputItem });
+        var output = await useCase.Execute(input);
+
+        output.Count.Should().Be(1);
+
+        var outputItem = output.Single();
+
+        //assert
+        outputItem.Score.Should().Be(5);
+    }
+
+    [Fact]
+    public async void CalcScoreUsingTransactionScriptShouldBe3()
+    {
+        //arrange 
+        var inputItem = new InputItem()
+        {
+            Key = Guid.NewGuid().ToString(),
+            NumOfDependents = 5,
+            FamilyIncome = 2000
+        };
+
+        var useCase = new ProcessDataUseCase(new ProcessDataWithTransactionScript());
+
+        //act
+        var input = new Input(new[] { inputItem });
+        var output = await useCase.Execute(input);
+
+        output.Count.Should().Be(1);
+
+        var outputItem = output.Single();
+
+        //assert
+        outputItem.Score.Should().Be(3);
+    }
+
+
+}
