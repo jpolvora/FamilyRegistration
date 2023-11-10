b261cff9210fb3685ad5f9967aa174da8c706feb
JONE POLVORA <jpolvora@gmail.com>
2023-11-10 06:00:28 -0400

    fix route controllers and catch errors

--- FamilyRegistration.Web/Controllers/UploadController.cs
+++ FamilyRegistration.Web/Controllers/UploadController.cs
@@ -4,8 +4,8 @@ using FamilyRegistration.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FamilyRegistration.Web.Controllers;
-[ApiController]
-[Route("/upload")]
+[ApiController()]
+[Route("[controller]/[action]")]
 public class UploadController : Controller
 {
     private readonly IProcessDataUseCase _useCase;
@@ -16,35 +16,43 @@ public class UploadController : Controller
     }
 
     [HttpPost()]
-    [Route("/upload-json")]
-
-    public async Task<IResult> Index(IFormFile formFile)
+    public async Task<IResult> JsonFile(IFormFile formFile)
     {
-        if (formFile != null && formFile.Length > 0)
+        if (formFile == null || formFile.Length == 0)
         {
-            using var fileStream = formFile.OpenReadStream();
-            var serializeOptions = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            };
-
-            var requestData = await JsonSerializer.DeserializeAsync<JsonFormatOne[]>(fileStream, serializeOptions);
-
-            if (requestData != null)
-            {
-                var input = requestData.Select(s => s.Adapt()).AsInput();
-
-                //instanciar useCase e executar
-                //o useCase fica responsável por coordenar as adaptações entre input e output da pipeline
-
-                var output = await _useCase.Execute(input);
-
-                //ordenar o output pelo Score mais alto
-                var result = output.OrderByDescending(x => x.Score).ToArray();
-
-                return TypedResults.Ok(result);
-            }
+            return TypedResults.BadRequest();
         }
-        return TypedResults.Ok();
+
+        using var fileStream = formFile.OpenReadStream();
+        var serializeOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        JsonFormatOne[]? jsonData = null;
+
+        try
+        {
+            jsonData = await JsonSerializer.DeserializeAsync<JsonFormatOne[]>(fileStream, serializeOptions);
+        }
+        catch
+        {
+            //invalid data json format file
+            return TypedResults.StatusCode(422);
+        }
+
+
+        var input = jsonData!.Select(s => s.Adapt()).AsInput();
+
+        //instanciar useCase e executar
+        //o useCase fica responsável por coordenar as adaptações entre input e output da pipeline
+
+        var output = await _useCase.Execute(input);
+
+        //ordenar o output pelo Score mais alto
+        var result = output.OrderByDescending(x => x.Score).ToArray();
+
+        return TypedResults.Ok(result);
+
     }
 }
